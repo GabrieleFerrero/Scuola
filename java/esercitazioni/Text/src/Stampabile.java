666fcbc8ca6ee86be1381d470e76e898104eb1bd
Gabriele Ferrero <63954224+GabrieleFerrero@users.noreply.github.com>
2021-10-05 22:43:37 +0200

    Delete java/esercitazioni directory

--- java/esercitazioni/Text/src/Stampabile.java
+++
@@ -1,4 +0,0 @@
-public interface Stampabile {
-    public abstract String mostra(); //se aprissi e chiudessi le graffe io implemento il metodo e non farei niente
-}
-
--- java/esercitazioni/Text/src/Testo.java
+++
@@ -1,7 +0,0 @@
-public class Testo implements Stampabile{
-    private String contenuto;
-
-    public void Testo(String contenuto){ }
-
-    public String mostra(){return "\""+contenuto+"\"";}
-}
